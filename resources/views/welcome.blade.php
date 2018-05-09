268a3bee940768f3ff0e191591bdbba434a10595
macbook pro yifan <yifan@magicmoma.com>
2018-05-09 21:33:22 +0800

    Procfile for Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - by Yifan
                 </div>
 
                 <div class="links">
